184864bdcfa8217638911a61ef616a30be466b0a
Danaz99 <108014934+Danaz99@users.noreply.github.com>
2022-06-25 19:37:49 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string heroName = "Angel";
+int heroPower = 150;
+string villainName = "Devil";
+int villainPower = 100;
+if (heroPower > villainPower)
+{
+    Console.WriteLine("villain damged");
+}
+
+else if (heroPower == villainPower)
+{
+    Console.WriteLine("oh..");
+}
+else if (heroPower < villainPower)
+{
+    Console.WriteLine("I am damged");
+}
+else
+{
+    Console.WriteLine("....");
+}
